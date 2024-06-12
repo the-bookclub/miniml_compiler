51a3d62c6197c8dfd91e8baa7ef18c708413d72e
JasonCubed132 <amcb2002@googlemail.com>
2024-06-12 23:22:39 +0100

    Resolve left recursion issues - AFAICT. Still needs fully implementing

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
-use pest::iterators::Pairs;
+use anyhow::Result;
+use pest::iterators::{Pair, Pairs};
 use pest::Parser;
 use pest_derive::Parser;
 
@@ -36,6 +37,40 @@ struct Variable {
     ident: String,
 }
 
-// fn parser(input: &str) -> IResult<&str, Expression> {
-//     MiniMLParser::parse(Rule::e_top, input)
-// }
+fn parser(input: &str) -> Result<Expression> {
+    let file = MiniMLParser::parse(Rule::file, input)
+        .expect("Bad parse")
+        .next()
+        .unwrap();
+
+    println!("{:?}", file);
+
+    transform_parse_output(file)
+}
+
+fn transform_parse_output(input: Pair<Rule>) -> Result<Expression> {
+    match input.as_rule() {
+        Rule::x => Ok(Expression::Var(Variable {
+            ident: input.as_span().as_str().to_string(),
+        })),
+        Rule::c_bool => match input.as_span().as_str() {
+            "true" => Ok(Expression::True),
+            "false" => Ok(Expression::False),
+            _ => {
+                panic!();
+            }
+        },
+        Rule::c_num => Ok(Expression::Num(input.as_span().as_str().parse().unwrap())),
+        _ => todo!(),
+    }
+}
+
+#[test]
+fn test_num() {
+    assert_eq!(parser("1").unwrap(), Expression::Num(1));
+}
+
+#[test]
+fn test_add() {
+    assert_eq!(parser("1+2").unwrap(), Expression::Num(1));
+}
